7112df765f706dacba9243d4d2446f58ac69fa12
Harshit Gupta <harshit9715@gmail.com>
2021-04-05 16:05:53 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -2,7 +2,7 @@
 
 recur() {
     cd "$1";
-    find . -name "*.zip" | xargs -P 5 -I fileName sh -c 'unzip -o -d "$(dirname "fileName")/$(basename -s .zip "fileName")" "fileName" && rm "fileName"'
+    find . -name "*.zip" | xargs -P 0 -I fileName sh -c 'unzip -o -d "$(dirname "fileName")/$(basename -s .zip "fileName")" "fileName" && rm "fileName"'
     for f in *; do
         if [ -d "$f" ]; then
             recur "$f"
@@ -12,9 +12,9 @@ recur() {
     return;
 }
 
-for i in "$@"; do
-    'unzip -o -d "$(dirname "$i")/$(basename -s .zip "$i")" "$i"'
-    recur "$i"
-done
-
 # execute as ./extract.sh 'Logs.zip'
+
+for i in "$@"; do
+    unzip -d "${i%.*}" "$i"
+	recur "${i%.*}"
+done
